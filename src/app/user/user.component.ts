e5371868bdf96e7c86763909a667a8906e0f6563
Chandan-prog <mishrachandan1997.cm@gmail.com>
2024-06-02 21:59:18 +0530

    adding multiple users and passing values

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component, signal, computed } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,27 +8,13 @@ const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  //selectedUser = DUMMY_USERS[randomIndex];  //this is called public property which is defined so that we can read dynamic data in html file
 
-  // get imagePath(){
-  //   return 'assets/users/' + this.selectedUser.avatar;
-  // }
+  @Input() avatar!:string
+  @Input() name!:string
 
-  // onSelectUser(){
-     // console.log(this.selectedUser.name);
-  //   const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
-  //   this.selectedUser = DUMMY_USERS[randomIndex];
-  // }
-
-  //Signals are more efficient than zone.js
-
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
-
-  onSelectUser(){
-    const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  get imagePath(){
+    return 'assets/users/'+this.avatar;
   }
-  
+
+  onSelectUser(){}
 }
