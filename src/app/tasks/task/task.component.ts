7791f4badf67d4d12cd25d9f3eabec900b336b66
Chandan-prog <mishrachandan1997.cm@gmail.com>
2024-06-04 20:45:20 +0530

    complete button of task functionality

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,21 +1,25 @@
-import { Component, Input, Type } from '@angular/core';
+import { Component, EventEmitter, Input, Output, Type } from '@angular/core';
 
 type Task = {
   id: string;
   userId: string;
   title: string;
-      summary:
-        string;
-      dueDate: string;
-}
+  summary: string;
+  dueDate: string;
+};
 @Component({
   selector: 'app-task',
   standalone: true,
   imports: [],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.css'
+  styleUrl: './task.component.css',
 })
 export class TaskComponent {
-  @Input({required: true}) task !: Task;
-  
+  @Input({ required: true }) task!: Task;
+
+  @Output() complete = new EventEmitter<string>();
+
+  onCompleteTask() {
+    this.complete.emit(this.task.id);
+  }
 }
