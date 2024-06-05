1255323768d149b3f6f13be98dbed7b34fe52309
Chandan-prog <mishrachandan1997.cm@gmail.com>
2024-06-05 12:54:09 +0530

    Adding services to use data globally

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,75 +1,83 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
-import { NewTaskComponent } from "./new-task/new-task.component";
-import { type NewTaskData } from './tasks.model';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css'],
-  imports: [TaskComponent, NewTaskComponent]
+  imports: [TaskComponent, NewTaskComponent],
 })
 export class TasksComponent {
   @Input({ required: true }) userId?: string;
   @Input({ required: true }) name?: string;
 
+  constructor(private tasksService: TasksService) {}
+
   //initial value of add-task-form
   isAddingTask = false;
 
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary: 'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary: 'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ];
+  // tasks = [
+  //   {
+  //     id: 't1',
+  //     userId: 'u1',
+  //     title: 'Master Angular',
+  //     summary: 'Learn all the basic and advanced features of Angular & how to apply them.',
+  //     dueDate: '2025-12-31',
+  //   },
+  //   {
+  //     id: 't2',
+  //     userId: 'u3',
+  //     title: 'Build first prototype',
+  //     summary: 'Build a first prototype of the online shop website',
+  //     dueDate: '2024-05-31',
+  //   },
+  //   {
+  //     id: 't3',
+  //     userId: 'u3',
+  //     title: 'Prepare issue template',
+  //     summary: 'Prepare and describe an issue template which will help with project management',
+  //     dueDate: '2024-06-15',
+  //   },
+  // ];
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userId);
+    // return this.tasks.filter((task) => task.userId === this.userId);
+    if (this.userId) {
+      return this.tasksService.getUserTasks(this.userId);
+    } else {
+      console.error('User ID is not defined.');
+      return;
+    }
   }
 
   onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    // this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
   onStartAddTask() {
     this.isAddingTask = true;
   }
 
-  onCancelAddTask() {
+  onCloseAddTask() {
     this.isAddingTask = false;
   }
 
-  onAddTask(taskData: NewTaskData) {
-    if (this.userId) {
-      this.tasks.unshift({
-        id: new Date().getTime().toString(),
-        userId: this.userId,
-        title: taskData.title,
-        summary: taskData.summary,
-        dueDate: taskData.dueDate
-      });
-      this.isAddingTask = false;
-    } else {
-      console.error("User ID is not defined.");
-    }
-  }
+  // onAddTask(taskData: NewTaskData) {
+    // if (this.userId) {
+    //   this.tasks.unshift({
+    //     id: new Date().getTime().toString(),
+    //     userId: this.userId,
+    //     title: taskData.title,
+    //     summary: taskData.summary,
+    //     dueDate: taskData.dueDate,
+    //   });
+    //   this.isAddingTask = false;
+    // } else {
+    //   console.error('User ID is not defined.');
+    // }
+   
+  // }
 }
-
--- src/app/tasks/tasks.model.ts
+++ src/app/tasks/tasks.model.ts
@@ -1,5 +1,5 @@
 export interface NewTaskData{
     title:string;
     summary:string;
-    dueDate:string;
+    date:string;
 }
